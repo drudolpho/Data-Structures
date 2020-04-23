37bd76925d7ce597fcd0b079885708486b84f9f7
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2020-04-23 19:08:11 -0400

    Day two tests passing

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -2,7 +2,7 @@
 # sys.path.append('../queue_and_stack')
 # from dll_queue import Queue
 # from dll_stack import Stack
-
+from _collections import deque
 
 class BinarySearchTree:
     def __init__(self, value):
@@ -68,17 +68,38 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node.left:
+            self.in_order_print(node.left)
+        print(node.value)
+        if node.right:
+            self.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        q = deque()
+        q.append(self)
+
+        while len(q) > 0:
+            current_node = q.popleft()
+            if current_node.left:
+                q.append(current_node.left)
+            if current_node.right:
+                q.append(current_node.right)
+            print(current_node.value)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = []
+        stack.append(self)
+        while len(stack) > 0:
+            current_node = stack.pop()
+            if current_node.right:
+                stack.append(current_node.right)
+            if current_node.left:
+                stack.append(current_node.left)
+            print(current_node.value)
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
